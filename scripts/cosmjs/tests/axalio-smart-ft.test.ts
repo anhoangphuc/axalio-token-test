716f355a9c1545e260361d0274fbca953945ad06
hoangphuc <hoangphucnb97@gmail.com>
2023-08-17 15:04:51 +0700

    AX-54 Add first test to test init success

--- scripts/cosmjs/tests/axalio-smart-ft.test.ts
+++ scripts/cosmjs/tests/axalio-smart-ft.test.ts
@@ -1,7 +1,19 @@
 import {expect} from "chai";
+import "dotenv/config";
+import {beforeEach} from "mocha";
+import {DirectSecp256k1HdWallet} from "@cosmjs/proto-signing";
+import {SigningStargateClient} from "@cosmjs/stargate";
 
 describe('Axalio smart ft', () => {
-    it('First test', () => {
-        expect(1 + 1).to.be.eq(2);
+    let wallet: DirectSecp256k1HdWallet;
+    let client: SigningStargateClient;
+    beforeEach(async () => {
+        wallet = await DirectSecp256k1HdWallet.fromMnemonic(process.env.ZNET_MNEMONIC || "");
+        client = await SigningStargateClient.connectWithSigner(process.env.ZNET_NODE || "", wallet);
+    });
+
+    it(`Init success`, async () => {
+        const chainId = await client.getChainId();
+        expect(chainId).to.be.eq("coreum-devnet-1");
     })
 })
