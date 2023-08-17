cbfcebe0af033097f000c329e5a02f3760dd5a9f
Sotatek-HaiTrieu2 <hai.trieu2@sotatek.com>
2023-08-17 09:35:12 +0700

    deploy script

--- scripts/cosmjs/scripts/deploy.ts
+++ scripts/cosmjs/scripts/deploy.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing"
 import { HdPath, stringToPath } from "@cosmjs/crypto"
-import { QueryClient, SigningStargateClient, StargateClient, createProtobufRpcClient, isMsgSendEncodeObject } from "@cosmjs/stargate";
-import { SigningCosmWasmClient, setupWasmExtension } from "@cosmjs/cosmwasm-stargate";
+import { GasPrice, QueryClient, SigningStargateClient, StargateClient, calculateFee, createProtobufRpcClient, decodeCosmosSdkDecFromProto, isMsgSendEncodeObject } from "@cosmjs/stargate";
+import { MsgStoreCodeEncodeObject, SigningCosmWasmClient, setupWasmExtension } from "@cosmjs/cosmwasm-stargate";
 import { InstantiateMsg } from "../ts/AxalioSmartFT.types";
-import { open, readFile } from "fs/promises";
+import { readFile } from "fs/promises";
+import { coreum, cosmos } from 'coreum';
+import { MsgInstantiateContract, MsgStoreCode } from 'cosmjs-types/cosmwasm/wasm/v1/tx';
 
 const coreumAccountPrefix = "testcore"; // the address prefix (different for different chains/environments)
 const coreumHDPath = "m/44'/990'/0'/0/0"; // coreum HD path (same for all chains/environments)
@@ -12,10 +14,6 @@ const coreumDenom = "utestcore"; // core denom (different for different chains/e
 const coreumRpcEndpoint = "https://full-node.testnet-1.coreum.dev:26657"; // rpc endpoint (different for different chains/environments)
 const senderMnemonic = process.env.MNEMONIC; // put mnemonic here
 
-// const httpClient = new HttpBatchClient(coreumRpcEndpoint);
-// const tendermintClient = await Tendermint34Client.create(httpClient);
-// const queryClient = QueryClient.withExtensions(tendermintClient, setupWasmExtension); 
-// const rpcClient = createProtobufRpcClient(queryClient);
 
 const main = async () => {
     const wallet = await generateKey(senderMnemonic)
@@ -23,26 +21,58 @@ const main = async () => {
     const client = await SigningStargateClient.connectWithSigner(coreumRpcEndpoint, wallet);
     console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
 
+    const { createRPCQueryClient, createRPCMsgClient } = coreum.ClientFactory;
+    const coreumClient = await createRPCQueryClient({ rpcEndpoint: coreumRpcEndpoint });
+
+    const fee = await coreumClient.coreum.feemodel.v1.minGasPrice({
+        afterBlocks: 10
+    })
+
+    console.log("Fee:", fee)
+
+
     console.log(
         "Alice balances:",
         await client.getAllBalances((await wallet.getAccounts())[0].address), // <-- replace with your generated address
     )
 
+    const gasPrice = GasPrice.fromString(`${decodeCosmosSdkDecFromProto(fee.minGasPrice.amount).toFloatApproximation()}${fee.minGasPrice.denom || ""}`);
+
     const senderClient = await SigningCosmWasmClient.connectWithSigner(
         coreumRpcEndpoint,
         wallet,
+        {
+            gasPrice,
+        }
     );
 
     const wasm = await readFile(__dirname + "/../../../artifacts/axalio_smart_ft.wasm");
 
-    const {codeId} = await senderClient.upload(sender.address, wasm, "auto", "Axalio-smart-ft")
+    // const a: MsgStoreCodeEncodeObject = {
+    //     typeUrl: "/cosmwasm.wasm.v1.MsgStoreCode",
+    //     value: MsgStoreCode.fromPartial({
+    //         wasmByteCode: wasm,
+    //         sender: sender.address,
+    //     })
+    // }
+
+    // const gasLimit = await senderClient.simulate(sender.address, [a], "Axalio-smart-ft")
+
+    // const { codeId } = await senderClient.upload(sender.address, wasm, calculateFee(gasLimit, gasPrice), "Axalio-smart-ft")
+    const { codeId } = await senderClient.upload(sender.address, wasm, "auto", "Axalio-smart-ft")
+
 
 
     // const codeId = 547; // Here we put the code id of the contract that we uploaded on the Coreum blockchain.
     const instantiateMsg: InstantiateMsg = { airdrop_amount: "1000000", initial_amount: "100000000", precision: 6, subunit: "uaxa", symbol: "AXA" };
-    const instantiateResult = await senderClient.instantiate(sender.address, codeId, instantiateMsg, "axalio-smart-ft", "auto", {});
+    const instantiateResult = await senderClient.instantiate(sender.address, codeId, instantiateMsg, "axalio-smart-ft", "auto", {
+        funds: [
+            { amount: "10000000", denom: coreumDenom }
+        ]
+    });
 
     console.log("Instantiate result:", { instantiateResult });
+
 
 }
 
